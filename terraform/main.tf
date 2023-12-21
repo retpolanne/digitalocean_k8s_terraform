66b7adb0ae186bdfb880f36f7620ce0545c25977
Anne Macedo <retpolanne@posteo.net>
2023-12-21 10:49:10 -0300

    Install istio

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,42 @@ provider "helm" {
   }
 }
 
+resource "kubernetes_namespace" "istio-system" {
+  metadata {
+    name = "istio-system"
+  }
+}
+
+resource "kubernetes_namespace" "istio-ingress" {
+  metadata {
+    name = "istio-ingress"
+  }
+}
+
+resource "helm_release" "istio-base" {
+  name       = "istio-base"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "base"
+  version    = "1.20.1"
+  namespace  = "istio-system"
+}
+
+resource "helm_release" "istiod" {
+  name       = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "istiod"
+  version    = "1.20.1"
+  namespace  = "istio-system"
+}
+
+resource "helm_release" "istio-ingress" {
+  name       = "istio-ingress"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "gateway"
+  version    = "1.20.1"
+  namespace  = "istio-ingress"
+}
+
 resource "helm_release" "superset" {
   name       = "superset"
   repository = "https://apache.github.io/superset"
